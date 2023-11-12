cb6269d15061598e61e07498c81def7fff308ee2
Kelly Loyd <kelly.loyd@icloud.com>
2023-11-12 10:19:39 -0600

    convenience shell file for assembly

---
+++ asm.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+vasm6502_oldstyle -dotdir -Fbin -wdc02 -L $1.lst $1.s
--- char_ex.s
+++ char_ex.s
@@ -13,11 +13,10 @@ out_hello_string:
   jmp out_hello_string
 
 done_hello:
+  lda #$0D     ; cr
+  jsr ECHO
+  
   jmp WOZENTRY
 
 hello: .asciiz "Welcome to Wozmon!"
 
-
-;0000: A2 00 B5 10 F0 07 20 EF FF E8 4C 02 00 4C 00 FF
-;0010: 57 65 6C 63 6F 6D 65 20 74 6F 20 57 6F 7A 6D 6F
-;0020: 6E 21 00 
